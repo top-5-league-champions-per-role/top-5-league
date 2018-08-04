6a177971e9cc7831f35d56d24b39f9cc8e68665b
toddnguyen47 <todd.nguyen47@gmail.com>
2018-08-04 10:43:53 -0700

    Hello

--- push_to_git.sh
+++ push_to_git.sh
@@ -2,7 +2,7 @@
 
 # -z switch will test the expansion of "$1" string
 # Basically, it will check if the "$1" expansion is a null string or not
-if [ -z "$1"]
+if [[ -z $1 ]]
 	then
 		echo "No arguments supplied to the script. Sample usage:"
 		echo "./push_to_git.sh \"Your commit message here\""
